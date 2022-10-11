5cd35aab506c40f97ba152a66be1b9be7c9ca6cf
Serkan Topal <serkan98topal@gmail.com>
2022-10-11 18:12:04 +0300

    fix

--- MvcProject/DataAccessLayer/Concrete/Context.cs
+++ MvcProject/DataAccessLayer/Concrete/Context.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer
+namespace DataAccessLayer.Concrete
 {
-    public class Class1
+    class Context
     {
     }
 }
