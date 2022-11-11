0e82e53c9f8782e38ce4d2d98e26cf7b52142255
Serkan Topal <serkan98topal@gmail.com>
2022-11-11 15:36:55 +0300

    Fix
    Code review

--- MvcProject/DataAccessLayer/Abstract/ICategoryDal.cs
+++ MvcProject/DataAccessLayer/Abstract/ICategoryDal.cs
@@ -9,12 +9,6 @@ namespace DataAccessLayer.Abstract
 {
     public interface ICategoryDal :IRepository<Category>
     {
-        //CRUD
-        List<Category> List();
-
-        void Insert(Category p);
-        void Update(Category p);
-        void Delete(Category p);
         
     }
 }
